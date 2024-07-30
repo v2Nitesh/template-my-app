f9bf54d145f5ca62e66177912ae8af63b6b9e18a
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2024-07-30 20:45:57 +0530

    Created sample code

---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Content() {
+  return (
+    <main className="content">
+      <h2>Content</h2>
+      <p>This is the main content area.</p>
+    </main>
+  );
+}
+
+export default Content;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <h2>Sidebar</h2>
+      <ul>
+        <li>
+          <a href="#section1">Section 1</a>
+        </li>
+        <li>
+          <a href="#section2">Section 2</a>
+        </li>
+        <li>
+          <a href="#section3">Section 3</a>
+        </li>
+      </ul>
+    </aside>
+  );
+}
+
+export default Sidebar;
